4bdb36e8f730ee12b9adad8e8ba5a2fe843613cb
redswm <redstel50@gmail.com>
2025-11-30 13:40:26 +0300

    Create numpad.js

---
+++ scripts/numpad.js
@@ -0,0 +1,21 @@
+// Слушатель событий для нажатия клавиш
+document.addEventListener('keydown', function(event) {
+    // Проверяем не зажат ли CTRL, SHIFT, ALT
+    if (event.ctrlKey || event.shiftKey || event.altKey) {
+        return;
+    }
+
+    // Numpad- Открывает уведомления
+    if (event.code === 'NumpadSubtract') {
+        event.preventDefault();
+        const notificationElement = document.querySelector('.--o-notification');
+        if (notificationElement) {
+            notificationElement.click();
+        }
+    }
+    // Numpad* Переход на страницу /crm/deal/
+    else if (event.code === 'NumpadMultiply') {
+        event.preventDefault();
+        window.location.href = '/crm/deal/';
+    }
+});
